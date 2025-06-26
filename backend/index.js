2219e4c22b0564b190d5128117132b1737999175
jashvanth370 <jashjazz11@gmail.com>
2025-06-26 14:58:16 +0530

    Project Initilize frontend and Backend

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+require('dotenv').config();
+
+const app= express();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=>{
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
+
