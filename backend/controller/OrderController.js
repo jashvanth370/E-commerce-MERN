d907321032c4ec015b0d100fd5f9a3478cfb3f7a
jashvanth370 <jashjazz11@gmail.com>
2025-06-29 11:57:14 +0530

    some order API added

--- backend/controller/OrderController.js
+++ backend/controller/OrderController.js
@@ -7,7 +7,7 @@ module.exports.createOrder = async (req, res, next) => {
         if (!orderItems || orderItems.length === 0) {
             return res.status(400).json({ message: 'No order items' });
         }
-        
+
         const order = new Order({
             user: req.user._id,
             orderItems,
@@ -17,9 +17,40 @@ module.exports.createOrder = async (req, res, next) => {
         });
 
         const createdOrder = await order.save();
-        res.status(201).json({orderDetails : createdOrder, message: "Order successfully created" , success: true});
+        res.status(201).json({ orderDetails: createdOrder, message: "Order successfully created", success: true });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 }
 
+module.exports.getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find();
+        if (!orders || orders.length === 0) {
+            return res.status(200).json({ message: "No orders " });
+        }
+        res.status(201).json({ orderDetails: orders, message: "Order successfully fetched", success: true });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports.getOrdersById = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        if (!userId) {
+            return res.status(404).json({ message: " User Not Found" });
+        }
+
+        const orders = await Order.find({user: userId});
+        if (!orders || orders.length === 0) {
+            return res.status(200).json({ message: "No orders for this user" });
+        }
+        res.status(201).json({ orderDetails: orders, message: "Order successfully fetched", success: true });
+
+    } catch (error) {
+        console.error("failed to fetch orders by user:", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,10 @@
-const { createOrder } = require('../controller/OrderController');
+const { createOrder, getOrdersById, getAllOrders } = require('../controller/OrderController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = require('express').Router();
 
 router.post('/createOrder',authMiddleware, createOrder);
+router.get('/getAllOrders',authMiddleware,getAllOrders)
+router.get('/getOrdersByUserId',authMiddleware, getOrdersById);
 
 module.exports = router;
